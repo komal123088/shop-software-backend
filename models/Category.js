7d1ee7c15dd27151af9106d19926528361e1c8f5
Komal Raza <komal123088@gmail.com>
2026-04-08 14:46:52 +0500

    fix: install mysql2

--- models/Category.js
+++ models/Category.js
@@ -1,31 +1,36 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/database');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
 
-const Category = sequelize.define('Category', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Category = sequelize.define(
+  "Category",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: { msg: "Category name is required" },
+        len: {
+          args: [2, 100],
+          msg: "Category name must be between 2 and 100 characters",
+        },
+      },
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
   },
-  name: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true,
-    validate: {
-      notEmpty: { msg: "Category name is required" },
-      len: { args: [2, 100], msg: "Category name must be between 2 and 100 characters" }
-    }
+  {
+    tableName: "categories",
+    timestamps: true,
+    indexes: [{ unique: true, fields: ["name"] }],
   },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  }
-}, {
-  tableName: 'categories',
-  timestamps: true,
-  indexes: [
-    { unique: true, fields: ['name'] }
-  ]
-});
+);
 
 module.exports = Category;
